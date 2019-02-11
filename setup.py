70175c5467cf7e4cb8e2a3b164ec21f6491ba9f8
RobMcH <admin@noobzunited.de>
2019-02-11 20:18:13 +0100

    Update license.
    Signed-off-by: RobMcH <admin@noobzunited.de>

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-# Automatically created by: scrapyd-deploy
-
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
-    name='mindfactory',
-    version='1.0',
+    name='mindfactory_crawling',
+    version='1.0.4',
+    author="Robert McHardy",
+    author_email="robert@robertmchardy.de",
+    description="A crawler for mindfactory.de",
+    url="https://github.com/RobMcH/mindfactory_crawling",
     packages=find_packages(),
-    entry_points={'scrapy': ['settings = mindfactory.settings']}, install_requires=['scrapy']
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={'scrapy': ['settings = mindfactory.settings']}, install_requires=['scrapy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
